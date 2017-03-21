dc884c5ca811d23e19ec10393461c522969e6908
MZacok <MZacok@seznam.cz>
2017-03-21 09:05:30 +0100

    Delete DELNICKYserver.js

--- DELNICKYserver.js
+++
@@ -1,9 +0,0 @@
-var http = require('http');
-var request = require('request');
-console.log('Nacteni konfigurace, databaze, lokalizace ap.');
-http.createServer(function (req, res) {
-  console.log('Zpracovani pozadavku z URL: ' + req.url);
-  if (req.url === "/DILO") {
-    res.end("<div>TOTO JE DIV SVETA</div>");
-  }
-}).listen(1337);
